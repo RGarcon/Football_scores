36a6d0fc51d6b9ead7a09beb7ccba9028d76815b
RGarcon <122042231+RGarcon@users.noreply.github.com>
2023-03-19 14:41:19 +0100

    Delete navbar.js

--- navbar.js
+++
@@ -1,9 +0,0 @@
-//Masquer nav quand Click bouton menu
-const menu = document.querySelector('.list-nav');
-const btnMenu = document.querySelector('.btn-toggle-contain');
-
-btnMenu.addEventListener('click', function (){
-
-    menu.classList.toggle('active-menu')
-
-})
